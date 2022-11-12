93e59d162a01b23a26402c965ffa596f3aee6f49
Samghoreyshi <samghoreyshi@outlook.com>
2022-11-12 22:37:04 +0330

    Files Added

---
+++ index.js
@@ -0,0 +1,23 @@
+// import { lookup, history } from 'yahoo-stocks';
+const yf = require('yahoo-stocks');
+const {lookup, history} = require('yahoo-stocks')
+const PORT = 5000
+const express = require('express');
+const app = express();
+const { response } = require('express');
+
+
+app.get('/results', function(req, res) {
+    
+    history('AAPL').then(response => {
+        console.log("Function Running Correctly")
+        res.json(response);
+
+    });
+
+
+})
+
+
+
+app.listen(PORT, () => console.log('SERVER RUNNING ON PORT: ' + PORT))
